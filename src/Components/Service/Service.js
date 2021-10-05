b63f55a915e414fe97e9d1edc34a0f48a38b4c01
srhimel <himusr@gmail.com>
2021-10-05 14:08:30 +0600

    Added a simple animation

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -12,7 +12,7 @@ const Service = (props) => {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 11V9a2 2 0 00-2-2m2 4v4a2 2 0 104 0v-1m-4-3H9m2 0h4m6 1a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     <span>{fee} Taka</span></div>
-                <div className="flex gap-2 bg-success px-2 rounded-full text-white items-center">
+                <div className="flex gap-2 px-2 badge badge-primary badge-lg items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Slider.css'
+import './Slider.css';
+import { Wave } from 'react-animated-text';
 const Slider = () => {
     return (
         <section className="slider text-center -mt-20 text-white px-4">
             <p className="text-2xl text-primary font-medium">Cooking is Easy, We Will Prove it to You!</p>
-            <p className="text-5xl font-bold">Great Cooking Class For You</p>
+            <p className="text-5xl font-bold"><Wave
+                effect="verticalFadeIn"
+                effectDirection="down"
+                iterations="1"
+                effectChange={2.0}
+                text="Great Cooking Class For You" /></p>
             <p className="md:w-1/2 mt-6">It's all about FOOD.Check out my recipes and let me know what you think about it. A healthy and easy way for foodies out there and I hope you people have Fun! </p>
             <div className="flex gap-6">
                 <Link to="/services">
-                    <button className="btn btn-success mt-8 tracking-widest px-8">Explore Cooking</button>
+                    <button className="btn btn-success mt-8 tracking-widest px-8 ">Explore Cooking</button>
                 </Link>
                 <Link to="/blogs">
                     <button className="btn btn-outline btn-primary px-8 mt-8 tracking-widest">Check feedback</button>
